5408b956aecbd0c49afca327fbc94bf780b3846b
Rodrigo Lau Ramos <darkbass16@gmail.com>
2024-11-13 19:46:04 +0000

    cambios finales

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".doms").classList.add(anyString());
 };
 
 let pronoun = ["the", "our", "tu"];
